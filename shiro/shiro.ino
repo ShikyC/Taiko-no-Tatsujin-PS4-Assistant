c6dac4dea52ffe2c58667f08c1d0dcf531625827
ShikyC <xz348@scarletmail.rutgers.edu>
2017-10-26 22:50:08 -0400

    Sheet change & optimization

--- shiro/shiro.ino
+++ shiro/shiro.ino
@@ -1,131 +1,37 @@
 //#include <avr/pgmspace.h>
 
-#define BUTTON_PRESS_TIME 20
+#define BUTTON_PRESS_TIME 15
+#define ROLL_COUNT 1 // Don't be too greedy
 
 //******************** Tips about the sheet file *******************
 // BPM value format: "B12345\n"
-// BPM value can't have more than 15 digits including the decimal. 
+// BPM value can't have more than 15 digits including the decimal.
 // For example, "B166.6667" is valid but "B166.66666666666667" is not.
-char* sheetFile = "B194\n"
-  "300020202020200020201000"
-  "100020202020111122221000"
-  "102020102010201020000000"
-  "100000000000100000000000"
-  "102020102010201020000000"
-  "100000000000100000100012"
-  "102020102010201020100010"
-  "100000100020102220122000"
-  "102020102010201020100010"
-  "100000100010202020102020"
-  "1"
-  "B193\n"
-  "0002"
-  "B192\n"
-  "0200"
-  "B191\n"
-  "0001"
-  "B184\n"
-  "0102"
-  "B183\n"
-  "0200"
-  "B182\n"
-  "0001"
-  "B181\n"
-  "0102"
-  "B178\n"
-  "0200"
-  "B176\n"
-  "0001"
-  "B162\n"
-  "0102"
-  "B150\n"
-  "0201"
-  "B140\n"
-  "0200"
-  "B120\n"
-  "0000000"
-  "3"
-  "B194\n"
-  "010201020112010"
-  "0020001000112010"
-  "0010202010112010"
-  "0010202010111010"
-  "0010201011201010"
-  "0020001011201010"
-  "0010202011201010"
-  "0010201101201111"
-  "0010201012112010"
-  "0020001000112010"
-  "2020112020112020"
-  "1011201020111110"
-  "0010201011201010"
-  "0020001101201010"
-  "0012201011201020"
-  "1222122211121112"
-  "0000300020102000"
-  "1000201020102000"
-  "1000201020002000"
-  "1011221022112220"
-  "1022221110102010"
-  "1020201020102000"
-  "1022221110102010"
-  "1222122210107700"
-  "100020002000100020002000"
-  "100020002000100020001020"
-  "100020002000100020002000"
-  "100020102020102010101000"
-  "1"
-  "B145.5\n"
-  "00200200122200100"
-  "100222111222111212"
-  "100212122122122122"
-  "100200222100200200"
-  "1"
-  "B194\n"
-  "11122227700111122227700"
-  "111122227700111122227700"
-  "100020002000100020202000"
-  "100020002000100020002000"
-  "100020002000100020002000"
-  "202020202000202020202000"
-  "202020202000202020202000"
-  "202220202000202220202000"
-  "20302020202020303000"
-  "100020100010100010001000"
-  "10002010001010001000"
-  "100000100010100010001000"
-  "100000100020100010221020"
-  "300010200020100010002000"
-  "101000200020100020001020"
-  "202010200010200010002000"
-  "10002010001020001022"
-  "100010200010200010002000"
-  "10001020001010002000"
-  "102210200020100020001000"
-  "102210201020102010201020"
-  "102210201020102222222222"
-  "100000000000"
-  "5555555555555555"
-  "5555555500112020"
-  "1210201010112210"
-  "1011221010221022"
-  "1000201011200010"
-  "1110201011201010"
-  "0010201011112222"
-  "1000201012221011"
-  "2211221122112211"
-  "2211221122112011"
-  "2011102011102011"
-  "1020111122221110"
-  "1220102012201020"
-  "1220102012201020"
-  "B145.5\n"
-  "101010101010"
-  "B194\n"
-  "1111111177777700"
-  "0030000000000000"
-  "\0"
-;
+// This is the sheet of 「前前前世」 (Not verified; might have mistakes)
+char* sheetFile = "B126.6666667\n"
+                  "404040404040"
+                  "B190\n"
+                  "1000100010102000100010001020100010001000101020001000100020102020"
+                  "1000100010102000100010002010200011102010200010005555555500000000"
+                  "1000201010002010001020001010200010002010100020100010200010102000"
+                  "1010202210102000101020112010200010002000101020100010200011102000"
+                  "1000201010002010001020101010200010002010100020100010200011102000"
+                  "1010202210102010101020221010222010002000101020100010200011102000"
+                  "1010202210102022101020221010222010002000100010200020101020112000"
+                  "1110201110201110201110201111200055555555550020100010201000112000"
+                  "1110201110201110201110201111200055555555550020100010201110102000"
+                  "3000001020000010001000102000000010000010200020000010001020000000"
+                  "10000010200000100010001020112000"
+                  "1010201110201010201120201110200010102011102010200011111110000000"
+                  "1110111011102011102011200020112011102022101020100010201110102010"
+                  "0010202210102220001020112010200011102011202011200011102000102220"
+                  "1110111011102011102011200020112011112022102011200010201110201120"
+                  "00102022102011200022102010221120001020001010201000102000"
+                  "B126.6666667\n"
+                  "211211"
+                  "B190\n"
+                  "2000001020002010001020112020222210205555555555555555550040400000\0"
+                  ;
 
 float bpm;
 int sheetCursor;
@@ -139,19 +45,17 @@ void setup() {
   pinMode(A2, OUTPUT); // A2 - left kat
   pinMode(A3, OUTPUT); // A3 - right kat
   pinMode(A4, INPUT); // A4 - start switch
-  
+
   sheetCursor = 0;
-  started = true;
-  
+  started = false;
+
   Serial.begin (9600);
-  delay(5000);
 }
 
 void loop() {
-  
+
   currentTime = millis();
   if (analogRead(A4) >= 1023) {
-    Serial.print("Started\n");
     started = true;
   }
   if (sheetCursor > strlen(sheetFile)) {
@@ -161,59 +65,64 @@ void loop() {
     return;
   }
   switch (sheetFile[sheetCursor]) {
-  case 'B':
-    bpmCursor = sheetCursor + 1;
-    while (sheetFile[bpmCursor] != '\n') {
-      bpmCursor++;
-    }
-    char bpmText[15];
-    strncpy(bpmText, &sheetFile[sheetCursor + 1], bpmCursor - sheetCursor - 1);
-    bpm = atof(bpmText);
-    sheetCursor = bpmCursor + 1;
-    return;
-  case '0':
-    delay (15000.0/bpm - millis() + currentTime);
-    sheetCursor++;
-    return;
-  case '1':
-    Serial.print ("x");
-    digitalWrite(A1, HIGH);
-    delay (BUTTON_PRESS_TIME - millis() + currentTime);
-    digitalWrite(A1, LOW);
-    delay (15000.0/bpm - BUTTON_PRESS_TIME);
-    sheetCursor++;
-    return;
-  case '2':
-    Serial.print ("o");
-    digitalWrite(A3, HIGH);
-    delay (BUTTON_PRESS_TIME - millis() + currentTime);
-    digitalWrite(A3, LOW);
-    delay (15000.0/bpm - BUTTON_PRESS_TIME);
-    sheetCursor++;
-    return;
-  case '3':
-    Serial.print ("X");
-    digitalWrite(A0, HIGH);
-    digitalWrite(A1, HIGH);
-    delay (BUTTON_PRESS_TIME - millis() + currentTime);
-    digitalWrite(A0, LOW);
-    digitalWrite(A1, HIGH);
-    delay (15000.0/bpm - BUTTON_PRESS_TIME);
-    sheetCursor++;
-    return;
-  case '4':
-    Serial.print ("O");
-    digitalWrite(A2, HIGH);
-    digitalWrite(A3, HIGH);
-    delay (BUTTON_PRESS_TIME - millis() + currentTime);
-    digitalWrite(A2, LOW);
-    digitalWrite(A3, LOW);
-    delay (15000.0/bpm - BUTTON_PRESS_TIME);
-    sheetCursor++;
-    return;
-  default:
-    delay (15000.0/bpm - millis() + currentTime);
-    sheetCursor++;
-    return;
+    case 'B':
+      bpmCursor = sheetCursor + 1;
+      while (sheetFile[bpmCursor] != '\n') {
+        bpmCursor++;
+      }
+      char bpmText[15];
+      strncpy(bpmText, &sheetFile[sheetCursor + 1], bpmCursor - sheetCursor - 1);
+      bpm = atof(bpmText);
+      sheetCursor = bpmCursor + 1;
+      return;
+    case '0': // Empty
+      delay (15000.0 / bpm - millis() + currentTime);
+      sheetCursor++;
+      return;
+    case '1': // small don
+      digitalWrite(A1, HIGH);
+      delay (BUTTON_PRESS_TIME);
+      digitalWrite(A1, LOW);
+      sheetCursor++;
+      delay (15000.0 / bpm - millis() + currentTime);
+      return;
+    case '2': // small kat
+      digitalWrite(A3, HIGH);
+      Serial.print ("o");
+      delay (BUTTON_PRESS_TIME);
+      digitalWrite(A3, LOW);
+      sheetCursor++;
+      delay (15000.0 / bpm - millis() + currentTime);
+      return;
+    case '3': // big don
+      digitalWrite(A0, HIGH);
+      digitalWrite(A1, HIGH);
+      Serial.print ("X");
+      delay (BUTTON_PRESS_TIME);
+      digitalWrite(A0, LOW);
+      digitalWrite(A1, HIGH);
+      sheetCursor++;
+      delay (15000.0 / bpm - millis() + currentTime);
+      return;
+    case '4': // big kat
+      digitalWrite(A2, HIGH);
+      digitalWrite(A3, HIGH);
+      Serial.print ("O");
+      delay (BUTTON_PRESS_TIME);
+      digitalWrite(A2, LOW);
+      digitalWrite(A3, LOW);
+      sheetCursor++;
+      delay (15000.0 / bpm - millis() + currentTime);
+      return;
+    case '5': // ROLL_COUNT
+      for (int i = 0; i < ROLL_COUNT; i++) {
+        digitalWrite(A1, HIGH);
+        delay (BUTTON_PRESS_TIME);
+        digitalWrite(A1, LOW);
+        delay (BUTTON_PRESS_TIME);
+      }
+      sheetCursor++;
+      delay (15000.0 / bpm - millis() + currentTime);
+      return;
   }
 }
