f104583781437f2864cf8febdadf3bc83f8e0f0a
ShikyC <zhangxunpx@gmail.com>
2017-10-30 18:21:36 -0400

    Minor bug fix and added some docs

--- shiro/shiro.ino
+++ shiro/shiro.ino
@@ -1,14 +1,36 @@
-//#include <avr/pgmspace.h>
-
 #define BUTTON_PRESS_TIME 35
 #define ROLL_COUNT 1 // Don't be too greedy
 
-//******************** Tips about the sheet file *******************
-// BPM value format: "BXXXXXXXX\n"
-// XXXXXXXX value can't have more than 15 digits including the decimal.
-// For example, "B166.6667" is valid but "B166.66666666666667" is not.
-// This is the sheet of 「紅」
-char* sheetFile = "B156.33\n"
+//********************** Tips about the sheet file *********************
+// To minimize the file I/O operations (which is not yet supported in
+// Arduino microprocessors unless implemented with external SD storage
+// libraries), the music sheet is preloded as a string.
+//
+// The format is similar with Taikosan Jiro files (.tja), but most of the 
+// tags are ommited. The only necessary tag is #BPM, and to save memory, 
+// it is in such format: 
+//                          "B" + "value" + "\n"
+// "value" can't have more than 15 digits including the decimal.
+// For example, "B166.6667\n" is valid, but "B166.6666666666667\n" is not, 
+// it's too long.
+//
+// You can use different characters to represent notes:
+//
+// Numerical digits (Jiro-like): 0, 1, 2, 3, 4, etc.
+// Alphabet characters: empty space, x, o, X, O, etc.
+//
+// "0" or " " - empty note
+// "1" or "x" - light don
+// "2" or "o" - light kat
+// "3" or "X" - heavy don
+// "4" or "O" - heavy kat
+// "5" or "r" or "R" - roll
+//
+// For convenience, you can use Jiro-type. However I *highly* recommend 
+// using the "ooxxOOXX" format, which is much easier to read and write.
+// For example, this is the sheet of "Kurenai".
+
+char* sheetFile = "B156.33\n" // Don't forget the line ending
                   "x x x x xxx xxxxx o o o o  o  o x x xxxxx x xxxxx o o o o  o  xx"
                   "x o xxo x ox xo x o xxo x ox xo x o xxo x ox xo x o xxooooxxx xx"
                   "x o x o xxo xxo x o x o xxo xxo x o x o xxo xxo x o xxx o xxx x "
@@ -34,7 +56,7 @@ char* sheetFile = "B156.33\n"
                   "x xxo xxo xxo o x xxo xxooxxo o x xxx x x xxx   "
                   "x x x x xxx xxx o x x x xxx xxx o xxx x o o xxx o xxx x o x xxx "
                   "xxxoooxxxoooxxx xxxoooxxxoooxxo xxxoooxxxoooxxx O"
-                  "\0";
+                  "\0"; // It's fine without explicitly adding the string ending '\0'
 
 float bpm;
 int sheetCursor;
@@ -63,21 +85,22 @@ void setup() {
   sheetTime = 0;
   started = false;
 
-  Serial.begin (9600);
+  Serial.begin(9600);
 }
 
 void loop() {
-  if (analogRead(A4) >= 10 && !started) {
+  // Temporary and convenient starting switch; will change in the future
+  if(analogRead(A4) >= 10 && !started) {
     started = true;
     startTime = micros();
   }
-  if (!started) {
+  if(!started) {
     return;
   }
-  switch (sheetFile[sheetCursor]) {
+  switch(sheetFile[sheetCursor]) {
     case 'B':
       bpmCursor = sheetCursor + 1;
-      while (sheetFile[bpmCursor] != '\n') {
+      while(sheetFile[bpmCursor] != '\n') {
         bpmCursor++;
       }
       char bpmText[15];
@@ -90,31 +113,31 @@ void loop() {
     case ' ': case '0':
       sheetTime += 1.5e7 / bpm;
       sheetCursor++;
-      longMicroDelay (sheetTime - micros() + startTime);
+      longMicroDelay(sheetTime - micros() + startTime);
       return;
       
     // light don
     case 'x': case '1':
       digitalWrite(A1, LOW);
       digitalWrite(LED_BUILTIN, HIGH);
-      delay (BUTTON_PRESS_TIME);
+      delay(BUTTON_PRESS_TIME);
       digitalWrite(A1, HIGH);
       digitalWrite(LED_BUILTIN, LOW);
       sheetTime += 1.5e7 / bpm;
       sheetCursor++;
-      longMicroDelay (sheetTime - micros() + startTime);
+      longMicroDelay(sheetTime - micros() + startTime);
       return;
 
     // light kat
     case 'o': case '2':
       digitalWrite(A3, LOW);
       digitalWrite(LED_BUILTIN, HIGH);
-      delay (BUTTON_PRESS_TIME);
+      delay(BUTTON_PRESS_TIME);
       digitalWrite(A3, HIGH);
       digitalWrite(LED_BUILTIN, LOW);
       sheetTime += 1.5e7 / bpm;
       sheetCursor++;
-      longMicroDelay (sheetTime - micros() + startTime);
+      longMicroDelay(sheetTime - micros() + startTime);
       return;
 
     // heavy don
@@ -122,13 +145,13 @@ void loop() {
       digitalWrite(A0, LOW);
       digitalWrite(A1, LOW);
       digitalWrite(LED_BUILTIN, HIGH);
-      delay (BUTTON_PRESS_TIME);
+      delay(BUTTON_PRESS_TIME);
       digitalWrite(A0, HIGH);
       digitalWrite(A1, HIGH);
       digitalWrite(LED_BUILTIN, LOW);
       sheetTime += 1.5e7 / bpm;
       sheetCursor++;
-      longMicroDelay (sheetTime - micros() + startTime);
+      longMicroDelay(sheetTime - micros() + startTime);
       return;
 
     // heavy kat
@@ -136,38 +159,42 @@ void loop() {
       digitalWrite(A2, LOW);
       digitalWrite(A3, LOW);
       digitalWrite(LED_BUILTIN, HIGH);
-      delay (BUTTON_PRESS_TIME);
+      delay(BUTTON_PRESS_TIME);
       digitalWrite(A2, HIGH);
       digitalWrite(A3, HIGH);
       digitalWrite(LED_BUILTIN, LOW);
       sheetTime += 1.5e7 / bpm;
       sheetCursor++;
-      longMicroDelay (sheetTime - micros() + startTime);
+      longMicroDelay(sheetTime - micros() + startTime);
       return;
 
     // Roll
     case 'r': case 'R': case '5':
-//      for (int i = 0; i < ROLL_COUNT; i++) {
-//        digitalWrite(A1, LOW);
-//        digitalWrite(LED_BUILTIN, HIGH);
-//        delay (BUTTON_PRESS_TIME);
-//        digitalWrite(A1, HIGH);
-//        digitalWrite(LED_BUILTIN, LOW);
-//        delay (BUTTON_PRESS_TIME);
-//      }
+      for(int i = 0; i < ROLL_COUNT; i++) {
+        digitalWrite(A1, LOW);
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(BUTTON_PRESS_TIME);
+        digitalWrite(A1, HIGH);
+        digitalWrite(A0, LOW);
+        delay(BUTTON_PRESS_TIME);
+        digitalWrite(A0, HIGH);
+      }
+      digitalWrite(LED_BUILTIN, LOW);
       sheetTime += 1.5e7 / bpm;
       sheetCursor++;
-      longMicroDelay (sheetTime - micros() + startTime);
+      longMicroDelay(sheetTime - micros() + startTime);
       return;
   }
   
-  if (sheetCursor > strlen(sheetFile)) {
+  if(sheetCursor > strlen(sheetFile)) {
     started = false;
     sheetCursor = 0;
   }
 }
 
-void longMicroDelay (float microTime) {
-  delay (microTime / 1000);
-  delayMicroseconds (microTime - floor(microTime / 1000) * 1000);
+// delayMicroseconds() doesn't support longer delay than 16383 microseconds, 
+// so we need to implement it by ourselves.
+void longMicroDelay(float microTime) {
+  delay(microTime / 1000);
+  delayMicroseconds(microTime - floor(microTime / 1000) * 1000);
 }
